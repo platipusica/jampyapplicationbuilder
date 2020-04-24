1b30d84415dc7ab5d94c3f5177532f06536c2ad8
platipusica <babicdd@gmail.com>
2020-04-24 09:36:11 +0800

    conv

---
+++ conv/index.php
@@ -0,0 +1,79 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Access database converter</title>
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1990 12:00:00 GMT" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="copyright" content="&copy; 2000-2020 D. Babic." />
+    <!-- Bootstrap 4.0 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+         integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+         crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+         integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+         crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="/css/style.css" />
+</head>
+<body>
+
+</header>
+    <main>
+      
+      
+        <h1>Access database to SQLite converter</h1>
+  <?php
+    if (isset($_SESSION['message']) && $_SESSION['message'])
+    {
+      printf('<div class="container-fluid">');
+      printf('<b>%s</b>', $_SESSION['message']);
+      printf('</div>');
+      unset($_SESSION['message']);
+         
+    }
+  ?>
+      <div id="page-content" class="container-fluid">   
+		<form action="upload.php" method="post"
+			enctype="multipart/form-data">
+
+			<div class="form-group row">
+				<div class="col-lg-4">
+					<label for="file">Database File:</label>
+				</div>
+				<div class="col-lg-8">
+					<input type="file" class="form-control" id="file" name="uploadedFile" />
+					<small id="fileHelp" class="form-text text-muted">Select MS Access database file to upload (db,mdb,accdb) (50MB max).</small>  
+					
+				</div>
+			</div>
+			<div class="form-group row">
+				<div class="col-lg-12 pull-right">
+					<button type="submit" name="uploadBtn" value="Upload" class="btn btn-primary">Upload and Convert</button>
+					<button type="reset" class="btn btn-secondary">Reset</button>
+				</div>
+			</div>
+		</form>
+	</div>
+  </main>
+  <footer id="footer">
+    <div class="container-fluid">
+        <hr/>
+<p><strong>About</strong></p>
+
+<p>This is an online Microsoft Access database conversion tool to convert old and new Access database formats to Sqlite database for now. It is built with <a href="http://jackcess.sourceforge.net">Jackess</a>, a Java library for reading and writing MS Access databases. It supports Access 97 and all versions 2000-2013. Depending on file size, the conversion might take a few minutes! Please wait to see: <b>File is successfully converted! Click on here to download the file.</b></p>
+            <span property="dc:creator">D. Babic</span>
+          </p>
+          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" alt="Creative Commons License" style="border-width:0" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+        </div>
+    </div>
+</footer>
+</body>
+</html>
+
---
+++ conv/upload.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+   function run_in_background($dest_path, $Priority = 1)
+   {
+       if($Priority)
+	       $PID = shell_exec("nohup nice -n 0 java -jar AccessConverter.jar --access-file $dest_path --task convert-sqlite 2> /dev/null & echo $!");
+       else
+           $PID = shell_exec("nohup $Command 2> /dev/null & echo $!");
+       return($PID);
+   }
+   function is_process_running($PID)
+   {
+       exec("ps $PID", $ProcessState);
+       return(count($ProcessState) >= 2);
+   }
+
+$message = ''; 
+if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
+{
+  if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
+  {
+    // get details of the uploaded file
+    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+    $fileName = $_FILES['uploadedFile']['name'];
+    $fileSize = $_FILES['uploadedFile']['size'];
+    $fileType = $_FILES['uploadedFile']['type'];
+    $fileNameCmps = explode(".", $fileName);
+    $fileExtension = strtolower(end($fileNameCmps));
+
+    // sanitize file-name
+    $newFileName = md5(time() . $fileName);
+    $newFileNameExt = $newFileName . '.' . $fileExtension;
+    $FileNameSqlite = $newFileName . '.sqlite3';
+
+    // check if file has one of the following extensions
+    $allowedfileExtensions = array('accdb', 'mdb', 'db');
+
+    if (in_array($fileExtension, $allowedfileExtensions))
+    {
+      // directory in which the uploaded file will be moved
+      $uploadFileDir = './upload/';
+      $dest_path = $uploadFileDir . $newFileNameExt;
+      //$_SESSION['dest_path'] = $dest_path;
+  
+  if(move_uploaded_file($fileTmpPath, $dest_path)) 
+
+	{
+	$ps = run_in_background($dest_path);
+	  while(is_process_running($ps))
+	   {
+		   echo("Running . ");
+
+		   ob_flush(); flush();
+		   sleep(1);
+	   }
+
+        $message ='File is successfully converted! Click on here to download the file.<a href="download.php?download_file='.$FileNameSqlite.'" title="Download File" </a>';
+      }
+      else 
+      {
+        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+      }
+    }
+    else
+    {
+      $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+    }
+  }
+  else
+  {
+    $message = 'There is some error in the file upload. Please check the following error.<br>';
+    $message .= 'Error:' . $_FILES['uploadedFile']['error'];
+  }
+}
+$_SESSION['message'] = $message;
+header("Location: index.php");
